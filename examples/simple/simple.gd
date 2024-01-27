fcc61f028cc84d666b40d68a1be0ffc649160edd
Tristan Strathearn <hello+tristan@hugemenace.co>
2024-01-27 20:16:20 +1100

    chore: tidy up the simple example scene's script

--- examples/simple/simple.gd
+++ examples/simple/simple.gd
@@ -7,26 +7,9 @@ extends Node2D
 # the event is triggered. This allows for less repetitive sounding events.
 
 
-var _sound_ready: bool = false
-
-
-func _ready() -> void:
-	# As the SoundManager requires some time to set things up behind the scenes,
-	# it's advised that you connect via its "loaded" event before triggering
-	# sounds. In this script we're using _sound_ready to mark it as ready.
-	SoundManager.loaded.connect(on_sound_manager_loaded)
-	
-
 func _input(p_event: InputEvent) -> void:
-	if not _sound_ready:
-		return
-		
 	if p_event.is_action_pressed("one"):
 		# To trigger an event, call the play function with the name of the 
 		# sound bank as the first parameter, followed by the name of the
 		# event. Every event requires at least one audio stream to work.
 		SoundManager.play("simple", "note")
-
-
-func on_sound_manager_loaded() -> void:
-	_sound_ready = true
