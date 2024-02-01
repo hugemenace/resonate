93ba4cc86c1ca6645b324c8d6caceeca3bb20a97
Tristan Strathearn <hello+tristan@hugemenace.co>
2024-02-01 20:54:29 +1100

    chore: tidy up the scene change examples

--- examples/scene_changes/scene_one.gd
+++ examples/scene_changes/scene_one.gd
@@ -3,46 +3,23 @@ extends Node2D
 
 @onready var timer: Timer = $Timer
 
-var _instance: PooledAudioStreamPlayer
-var _music_playing: bool
+var _is_playing_music: bool
 
 
 func _ready():
 	MusicManager.loaded.connect(on_music_manager_updated)
 	MusicManager.banks_updated.connect(on_music_manager_updated)
-	
-	SoundManager.loaded.connect(on_sound_manager_updated)
-	SoundManager.banks_updated.connect(on_sound_manager_updated)
-	
-	timer.timeout.connect(on_timer_timeout)
-
-
-func _exit_tree():
-	if _instance == null:
-		return
-		
-	_instance.release()
 
 
 func on_music_manager_updated() -> void:
-	if _music_playing or not MusicManager.has_loaded:
+	if _is_playing_music or not MusicManager.has_loaded:
 		return
 	
-	MusicManager.set_volume(-10)
-	MusicManager.play("scene_one", "track_a", 0)
+	MusicManager.play("scene_one", "track_a")
 	
-	_music_playing = true
+	# Calling auto_stop here ensures that when this particular node exits
+	# the scene tree the music track stops playing. This would be 
+	# equivalent to stopping the track in _exit_tree or @tree_exiting.
+	MusicManager.auto_stop(self, "scene_one", "track_a")
 	
-	
-func on_sound_manager_updated() -> void:
-	if _instance != null or not SoundManager.has_loaded:
-		return
-		
-	_instance = SoundManager.instance("scene_one", "note")
-	
-
-func on_timer_timeout() -> void:
-	if _instance == null or not SoundManager.has_loaded:
-		return
-		
-	_instance.trigger()
+	_is_playing_music = true
--- examples/scene_changes/scene_two.gd
+++ examples/scene_changes/scene_two.gd
@@ -4,42 +4,26 @@ extends Node2D
 @onready var timer: Timer = $Timer
 
 var _instance: PooledAudioStreamPlayer
-var _music_playing: bool
 
 
 func _ready():
-	MusicManager.loaded.connect(on_music_manager_updated)
-	MusicManager.banks_updated.connect(on_music_manager_updated)
-	
 	SoundManager.loaded.connect(on_sound_manager_updated)
 	SoundManager.banks_updated.connect(on_sound_manager_updated)
 	
 	timer.timeout.connect(on_timer_timeout)
 
 
-func _exit_tree():
-	if _instance == null:
-		return
-		
-	_instance.release()
-	
-
-func on_music_manager_updated() -> void:
-	if _music_playing or not MusicManager.has_loaded:
-		return
-		
-	MusicManager.set_volume(-10)
-	MusicManager.play("scene_two", "track_a", 0)
-	
-	_music_playing = true
-	
-	
 func on_sound_manager_updated() -> void:
 	if _instance != null or not SoundManager.has_loaded:
 		return
 		
 	_instance = SoundManager.instance("scene_two", "note")
 	
+	# Calling auto_release here ensures that when this particular node exits
+	# the scene tree the instance is automatically released. This would be
+	# equivalent to releasing the instance in _exit_tree or @tree_exiting.
+	SoundManager.auto_release(self, _instance)
+
 
 func on_timer_timeout() -> void:
 	if _instance == null or not SoundManager.has_loaded:
