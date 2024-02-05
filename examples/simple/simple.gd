37cbfcad4c57277f47bfd2a4d3e742f7dfab4da0
Tristan Strathearn <hello+tristan@hugemenace.co>
2024-02-05 19:53:31 +1100

    chore: add a play_varied example to the 2D scene

--- examples/simple/simple.gd
+++ examples/simple/simple.gd
@@ -13,3 +13,9 @@ func _input(p_event: InputEvent) -> void:
 		# sound bank as the first parameter, followed by the name of the
 		# event. Every event requires at least one audio stream to work.
 		SoundManager.play("simple", "note")
+		
+	if p_event.is_action_pressed("two"):
+		# To trigger an event with varied pitch and volume, call the play_varied
+		# function with an additional pitch and volume setting. These will only
+		# apply to play_varied, with further calls to play using the defaults.
+		SoundManager.play_varied("simple", "note", randf_range(0.8, 1.2), randf_range(-8, 0))
