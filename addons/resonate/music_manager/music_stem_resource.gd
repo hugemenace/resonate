f80af46f43673b0d536740806d9c1cf1513dc64b
Tristan Strathearn <hello+tristan@hugemenace.co>
2024-02-06 19:22:54 +1100

    chore: update music stem and track resource variable defaults

--- addons/resonate/music_manager/music_stem_resource.gd
+++ addons/resonate/music_manager/music_stem_resource.gd
@@ -2,13 +2,7 @@ class_name MusicStemResource
 extends Resource
 
 
-@export var name: String
-@export var enabled: bool
-@export_range(-80.0, 6.0, 0.1, "suffix:dB") var volume: float
+@export var name: String = ""
+@export var enabled: bool = false
+@export_range(-80.0, 6.0, 0.1, "suffix:dB") var volume: float = 0.0
 @export var stream: AudioStream
-
-
-func _init(p_name := "", p_enabled := true, p_stream: AudioStream = null):
-	name = p_name
-	enabled = p_enabled
-	stream = p_stream
--- addons/resonate/music_manager/music_track_resource.gd
+++ addons/resonate/music_manager/music_track_resource.gd
@@ -2,11 +2,6 @@ class_name MusicTrackResource
 extends Resource
 
 
-@export var name: String
-@export var bus: String
+@export var name: String = ""
+@export var bus: String = ""
 @export var stems: Array[MusicStemResource]
-
-
-func _init(p_name := "", p_stems := []):
-	name = p_name
-	stems.assign(p_stems)
